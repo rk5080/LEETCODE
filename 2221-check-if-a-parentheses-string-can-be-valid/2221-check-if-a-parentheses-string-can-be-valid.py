914e8048160a287749801599836f07485aa6bd60
rk5080 <113831469+rk5080@users.noreply.github.com>
2025-01-13 00:30:40 +0530

    Time: 178 ms (43.48%), Space: 16.9 MB (21.74%) - LeetHub

---
+++ 2221-check-if-a-parentheses-string-can-be-valid/2221-check-if-a-parentheses-string-can-be-valid.py
@@ -0,0 +1,34 @@
+class Solution(object):
+    def canBeValid(self, s, locked):
+        """
+        :type s: str
+        :type locked: str
+        :rtype: bool
+        """
+        n = len(s)
+        if n % 2 != 0: return False
+        balance = 0
+        available = 0
+        for i in range(n):
+            if locked[i] == '0':
+                available += 1
+            else:
+                if s[i] == '(':
+                    balance += 1
+                else:
+                    balance -= 1
+            if balance + available < 0:
+                return False
+        balance = 0
+        available = 0
+        for i in range(n-1, -1, -1):
+            if locked[i] == '0':
+                available += 1
+            else:
+                if s[i] == ')':
+                    balance += 1
+                else:
+                    balance -= 1
+            if balance + available < 0:
+                return False
+        return True
