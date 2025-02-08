e553306716ac83551aa4fa1511fc377b9428e277
rk5080 <113831469+rk5080@users.noreply.github.com>
2025-02-09 01:45:12 +0530

    Time: 35 ms (33.78%), Space: 19.6 MB (27.03%) - LeetHub

---
+++ 2090-number-of-ways-to-arrive-at-destination/2090-number-of-ways-to-arrive-at-destination.py
@@ -0,0 +1,42 @@
+class Solution(object):
+    def countPaths(self, n, roads):
+        """
+        :type n: int
+        :type roads: List[List[int]]
+        :rtype: int
+        """
+        graph = collections.defaultdict(list)
+        for u, v, t in roads:
+            graph[u].append((v, t))
+            graph[v].append((u, t))
+            
+        #2. times, ways array initializer
+        times = [float('inf')] * n
+        ways = [0] * n
+        
+        times[0] = 0
+        ways[0] = 1
+        
+        #3. dijkstra
+        pq = [(0, 0)] # time, node
+        
+        while pq:
+            old_t, u = heapq.heappop(pq) # current time, current node
+            
+            for v, t in graph[u]:
+                new_t = old_t + t
+                
+                # casual logic: update shorter path
+                if new_t < times[v]:
+                    times[v] = new_t
+                    ways[v] = ways[u]
+                    heapq.heappush(pq, (new_t, v))
+                
+                # if find same time path... update ways only
+                elif new_t == times[v]:
+                    ways[v] += ways[u]
+                
+        modulo = 10 ** 9 + 7
+        
+        return ways[n-1] % modulo
+        
